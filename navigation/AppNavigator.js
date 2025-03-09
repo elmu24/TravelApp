2e19612bf4524064bb623864e4f3ca3154332412
elmu24 <elena.mueller@muebu.eu>
2025-03-09 20:25:36 +0200

    improve AppNavigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,32 +1,39 @@
 import React, { useContext } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { Pressable } from 'react-native';  // 使用 Pressable 替代 TouchableOpacity
+import { Pressable } from 'react-native';  
 import { AuthContext } from "../context/AuthContext";
 import BottomTabNavigator from "./BottomTabNavigator";
 import LoginScreen from "../screens/LoginScreen";
 import { Ionicons } from '@expo/vector-icons';
 
+// create stack navigator component
 const Stack = createStackNavigator();
 
+
+// Creating navigation -> login screen -> menu
 const AppNavigator = () => {
+  // Get user state and logout function from AuthContext
   const { user, logout } = useContext(AuthContext);
 
+  // Layout and Sytle
   return (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
           headerStyle: {
-            backgroundColor: '#8A9FD4', // Light blue-purple color
+            backgroundColor: '#8A9FD4', 
           },
-          headerTintColor: '#fff', // White text
+          headerTintColor: '#fff', 
           headerTitleStyle: {
             color: '#fff',
           },
         }}
       >
+        {/* If the user is NOT logged in, show the LoginScreen */}
         {!user ? (
           <Stack.Screen name="Login" component={LoginScreen} />
+          // If the user is logged in, show the main app with the logout button
         ) : (
           <Stack.Screen 
             name="MainApp" 
